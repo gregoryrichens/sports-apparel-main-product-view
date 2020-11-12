1f841178375f38c3a0e39d667875983cd0bd1408
Gregory Richens <gregoryrichens@gmail.com>
2020-11-12 01:16:22 -0700

    added seed data file for seed script

---
+++ client/seed-data.js
@@ -0,0 +1,185 @@
+module.exports = [
+  {
+    productId: 'NMD_R1',
+    itemId: 'NMD_R1_black',
+    color: 'Grey Four / Grey Four / Core Black',
+    price: 140,
+    salePrice: 122,
+    stars: 5,
+    numRatings: 394,
+    images: [
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/e1b02183615f4f559c6cab65011a4ea0_9366/NMD_R1_Shoes_Grey_FV1733_01_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/e1b02183615f4f559c6cab65011a4ea0_9366/NMD_R1_Shoes_Grey_FV1733_01_standard.jpg',
+        description: 'main view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/ae137586e7414ce29c3dab88014f8291_9366/NMD_R1_Shoes_Grey_FV1733_010_hover_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/ae137586e7414ce29c3dab88014f8291_9366/NMD_R1_Shoes_Grey_FV1733_010_hover_standard.jpg',
+        description: 'rear leg view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/03c1ae23712643d58115ab65011a5cdf_9366/NMD_R1_Shoes_Grey_FV1733_02_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/03c1ae23712643d58115ab65011a5cdf_9366/NMD_R1_Shoes_Grey_FV1733_02_standard.jpg',
+        description: 'top down shoe view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/9ce44ab246c44d799274ab65011a6413_9366/NMD_R1_Shoes_Grey_FV1733_03_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/9ce44ab246c44d799274ab65011a6413_9366/NMD_R1_Shoes_Grey_FV1733_03_standard.jpg',
+        description: 'sole view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/20f4fd77c651428bafaeab65011a6b2b_9366/NMD_R1_Shoes_Grey_FV1733_04_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/20f4fd77c651428bafaeab65011a6b2b_9366/NMD_R1_Shoes_Grey_FV1733_04_standard.jpg',
+        description: 'front right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/8d1620a2e9aa48649808ab65011a732d_9366/NMD_R1_Shoes_Grey_FV1733_05_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/8d1620a2e9aa48649808ab65011a732d_9366/NMD_R1_Shoes_Grey_FV1733_05_standard.jpg',
+        description: 'back right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/fc47eef6b2424b97888eab65011a55a5_9366/NMD_R1_Shoes_Grey_FV1733_06_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/fc47eef6b2424b97888eab65011a55a5_9366/NMD_R1_Shoes_Grey_FV1733_06_standard.jpg',
+        description: 'inside side view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/68c2e31e971c4d8688b8ab65011d202b_9366/NMD_R1_Shoes_Grey_FV1733_41_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/68c2e31e971c4d8688b8ab65011d202b_9366/NMD_R1_Shoes_Grey_FV1733_41_detail.jpg',
+        description: 'partial top'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/5e4412385eef4aca8af0ab65011d279b_9366/NMD_R1_Shoes_Grey_FV1733_42_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/5e4412385eef4aca8af0ab65011d279b_9366/NMD_R1_Shoes_Grey_FV1733_42_detail.jpg',
+        description: 'partial sole'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/3d6cec5181fb47939b97ab65011d2db9_9366/NMD_R1_Shoes_Grey_FV1733_43_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/3d6cec5181fb47939b97ab65011d2db9_9366/NMD_R1_Shoes_Grey_FV1733_43_detail.jpg',
+        description: 'partial side'
+      }
+    ]
+  },
+  {
+    productId: 'NMD_R1',
+    itemId: 'NMD_R1_rwb',
+    color: 'Collegiate Navy / Scarlet / Cloud White',
+    price: 140,
+    salePrice: null,
+    stars: 5,
+    numRatings: 394,
+    images: [
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/73101ab9d9ee445db281ab57011a0229_9366/NMD_R1_Shoes_Blue_FV1734_01_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/73101ab9d9ee445db281ab57011a0229_9366/NMD_R1_Shoes_Blue_FV1734_01_standard.jpg',
+        description: 'main view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/93a4bcc32f2e4be889dfab6700ec361a_9366/NMD_R1_Shoes_Blue_FV1734_010_hover_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/93a4bcc32f2e4be889dfab6700ec361a_9366/NMD_R1_Shoes_Blue_FV1734_010_hover_standard.jpg',
+        description: 'rear leg view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/784ad9f9a7cc420f9e42ab57011a0490_9366/NMD_R1_Shoes_Blue_FV1734_02_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/784ad9f9a7cc420f9e42ab57011a0490_9366/NMD_R1_Shoes_Blue_FV1734_02_standard.jpg',
+        description: 'top down shoe view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/cc0d2945469644809ea0ab57011a0624_9366/NMD_R1_Shoes_Blue_FV1734_03_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/cc0d2945469644809ea0ab57011a0624_9366/NMD_R1_Shoes_Blue_FV1734_03_standard.jpg',
+        description: 'sole view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/9f7710819c5741f9a21cab57011a0738_9366/NMD_R1_Shoes_Blue_FV1734_04_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/9f7710819c5741f9a21cab57011a0738_9366/NMD_R1_Shoes_Blue_FV1734_04_standard.jpg',
+        description: 'front right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/e778ed9c2cff46e1967dab57011a08d3_9366/NMD_R1_Shoes_Blue_FV1734_05_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/e778ed9c2cff46e1967dab57011a08d3_9366/NMD_R1_Shoes_Blue_FV1734_05_standard.jpg',
+        description: 'back right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/3457df745b7b45eab18fab57011a034d_9366/NMD_R1_Shoes_Blue_FV1734_06_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/3457df745b7b45eab18fab57011a034d_9366/NMD_R1_Shoes_Blue_FV1734_06_standard.jpg',
+        description: 'inside side view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/851e17cc73f8475193b6ab57011a0a44_9366/NMD_R1_Shoes_Blue_FV1734_41_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/851e17cc73f8475193b6ab57011a0a44_9366/NMD_R1_Shoes_Blue_FV1734_41_detail.jpg',
+        description: 'partial top'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/8ddfdba0f1dd4d45b648ab57011a0b56_9366/NMD_R1_Shoes_Blue_FV1734_42_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/8ddfdba0f1dd4d45b648ab57011a0b56_9366/NMD_R1_Shoes_Blue_FV1734_42_detail.jpg',
+        description: 'partial sole'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/c2232c25e02847b5ae70ab57011a0cf5_9366/NMD_R1_Shoes_Blue_FV1734_43_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/c2232c25e02847b5ae70ab57011a0cf5_9366/NMD_R1_Shoes_Blue_FV1734_43_detail.jpg',
+        description: 'partial side'
+      }
+    ]
+  },
+  {
+    productId: 'NMD_R1',
+    itemId: 'NMD_R1_purple',
+    color: 'Core Black / Supplier Colour / Core Black',
+    price: 140,
+    salePrice: 122,
+    stars: 5,
+    numRatings: 394,
+    images: [
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/7967fa1a9cb742d8a437ab6501290681_9366/NMD_R1_Shoes_Black_FV8732_01_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/7967fa1a9cb742d8a437ab6501290681_9366/NMD_R1_Shoes_Black_FV8732_01_standard.jpg',
+        description: 'main view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/cbc46e26b72348de96b9ab6700ec7151_9366/NMD_R1_Shoes_Black_FV8732_010_hover_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/cbc46e26b72348de96b9ab6700ec7151_9366/NMD_R1_Shoes_Black_FV8732_010_hover_standard.jpg',
+        description: 'front leg view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/159c3a98d97c4450befaab6501291547_9366/NMD_R1_Shoes_Black_FV8732_02_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/159c3a98d97c4450befaab6501291547_9366/NMD_R1_Shoes_Black_FV8732_02_standard.jpg',
+        description: 'top down shoe view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/a34cea7a8a2a428c866aab6501291b52_9366/NMD_R1_Shoes_Black_FV8732_03_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/a34cea7a8a2a428c866aab6501291b52_9366/NMD_R1_Shoes_Black_FV8732_03_standard.jpg',
+        description: 'sole view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/efaca33fa4c64993af53ab65012921db_9366/NMD_R1_Shoes_Black_FV8732_04_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/efaca33fa4c64993af53ab65012921db_9366/NMD_R1_Shoes_Black_FV8732_04_standard.jpg',
+        description: 'front right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/d90bb5490ce742818b7eab6501292a8b_9366/NMD_R1_Shoes_Black_FV8732_05_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/d90bb5490ce742818b7eab6501292a8b_9366/NMD_R1_Shoes_Black_FV8732_05_standard.jpg',
+        description: 'back right profile'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/1533834830b342eeab3bab6501290e3c_9366/NMD_R1_Shoes_Black_FV8732_06_standard.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/1533834830b342eeab3bab6501290e3c_9366/NMD_R1_Shoes_Black_FV8732_06_standard.jpg',
+        description: 'inside side view'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/19f72759451242ab8af5ab6501293307_9366/NMD_R1_Shoes_Black_FV8732_41_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/19f72759451242ab8af5ab6501293307_9366/NMD_R1_Shoes_Black_FV8732_41_detail.jpg',
+        description: 'partial side'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/117a8af46a804575b8bfab650129408b_9366/NMD_R1_Shoes_Black_FV8732_43_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/117a8af46a804575b8bfab650129408b_9366/NMD_R1_Shoes_Black_FV8732_43_detail.jpg',
+        description: 'partial sole'
+      },
+      {
+        mainUrl: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/ce17402365f349d091acab65012939cd_9366/NMD_R1_Shoes_Black_FV8732_42_detail.jpg',
+        thumbnailUrl: 'https://assets.adidas.com/images/h_32,f_auto,q_auto:sensitive,fl_lossy/ce17402365f349d091acab65012939cd_9366/NMD_R1_Shoes_Black_FV8732_42_detail.jpg',
+        description: 'partial back'
+      }
+    ]
+  }
+];
