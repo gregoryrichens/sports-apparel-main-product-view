1d3095c53902808d99b33d20382e75620f0723ec
Gregory Richens <gregoryrichens@gmail.com>
2020-11-21 20:31:43 -0700

    added logic for rendering based on productID

--- client/index.jsx
+++ client/index.jsx
@@ -4,8 +4,15 @@ import axios from 'axios';
 import App from './components/App.jsx';
 
 // const dummyData = require('../dummyData.js');
+const url = (document.URL);
+const parseURL = url.split('/');
+const itemID = parseURL[parseURL.length - 1];
 
-axios.get('/api/products/21/trackpinch')
+const addressBarID = Number(itemID);
+
+const productID = addressBarID || 21;
+
+axios.get(`/api/products/${productID}/trackpinch`)
   .then((response) => {
     ReactDOM.render(<App data={response.data[0]}/>, document.getElementById('TrackPinch'));
   })
