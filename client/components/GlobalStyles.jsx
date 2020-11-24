19fd4d2720e862f41a41e01661c8bc9f4435f1f6
Gregory Richens <gregoryrichens@gmail.com>
2020-11-24 15:09:13 -0700

    added global style from body into globalstyle component

---
+++ client/components/GlobalStyles.jsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: AdihausDIN,Helvetica,Arial,sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    background-color: #fff;
+    color: #000;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    overflow-y: scroll;
+    padding: 0;
+    text-rendering: optimizeLegibility;
+    text-transform: none;
+  }
+`;
+
+export default GlobalStyle;
